24534ecd6926e43ac988777f4a95c2d76d766ee4
Qrac <qrac.jp@gmail.com>
2020-07-09 21:42:41 +0900

    Clean up

--- functions/open-graph.php
+++ functions/open-graph.php
@@ -1,22 +1,19 @@
 <?php
-/*
-  Copyright 2010 Scott MacVicar
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+/**
+ * Copyright 2010 Scott MacVicar
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * Original can be found at https://github.com/scottmac/opengraph/blob/master/OpenGraph.php
+ */
 
-  Original can be found at https://github.com/scottmac/opengraph/blob/master/OpenGraph.php
-
-*/
-
-class OpenGraph implements Iterator
-{
+class OpenGraph implements Iterator {
   /**
    * There are base schema's based on type, this is just
    * a map so that the schema can be obtained
@@ -47,13 +44,13 @@ class OpenGraph implements Iterator
    * @return OpenGraph
    */
   static public function fetch($URI) {
-        $response = wp_remote_get($URI);
+    $response = wp_remote_get($URI);
 
-        if (!empty($response)) {
-            return self::_parse($response['body']);
-        } else {
-            return false;
-        }
+    if (!empty($response)) {
+      return self::_parse($response['body']);
+    } else {
+      return false;
+    }
   }
 
   /**
@@ -83,48 +80,48 @@ class OpenGraph implements Iterator
     $nonOgDescription = null;
 
     foreach ($tags AS $tag) {
-      if ($tag->hasAttribute('property') &&
-          strpos($tag->getAttribute('property'), 'og:') === 0) {
+      if ($tag->hasAttribute('property') && strpos($tag->getAttribute('property'), 'og:') === 0) {
         $key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
         $page->_values[$key] = $tag->getAttribute('content');
       }
 
       //Added this if loop to retrieve description values from sites like the New York Times who have malformed it.
-      if ($tag ->hasAttribute('value') && $tag->hasAttribute('property') &&
-          strpos($tag->getAttribute('property'), 'og:') === 0) {
+      if ($tag ->hasAttribute('value') && $tag->hasAttribute('property') && strpos($tag->getAttribute('property'), 'og:') === 0) {
         $key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
         $page->_values[$key] = $tag->getAttribute('value');
       }
+
       //Based on modifications at https://github.com/bashofmann/opengraph/blob/master/src/OpenGraph/OpenGraph.php
       if ($tag->hasAttribute('name') && $tag->getAttribute('name') === 'description') {
-                $nonOgDescription = $tag->getAttribute('content');
-            }
-
+        $nonOgDescription = $tag->getAttribute('content');
+      }
     }
+
     //Based on modifications at https://github.com/bashofmann/opengraph/blob/master/src/OpenGraph/OpenGraph.php
     if (!isset($page->_values['title'])) {
-            $titles = $doc->getElementsByTagName('title');
-            if ($titles->length > 0) {
-                $page->_values['title'] = $titles->item(0)->textContent;
-            }
-        }
-        if (!isset($page->_values['description']) && $nonOgDescription) {
-            $page->_values['description'] = $nonOgDescription;
-        }
+      $titles = $doc->getElementsByTagName('title');
+      if ($titles->length > 0) {
+        $page->_values['title'] = $titles->item(0)->textContent;
+      }
+    }
 
-        //Fallback to use image_src if ogp::image isn't set.
-        if (!isset($page->values['image'])) {
-            $domxpath = new DOMXPath($doc);
-            $elements = $domxpath->query("//link[@rel='image_src']");
+    if (!isset($page->_values['description']) && $nonOgDescription) {
+      $page->_values['description'] = $nonOgDescription;
+    }
 
-            if ($elements->length > 0) {
-                $domattr = $elements->item(0)->attributes->getNamedItem('href');
-                if ($domattr) {
-                    $page->_values['image'] = $domattr->value;
-                    $page->_values['image_src'] = $domattr->value;
-                }
-            }
+    //Fallback to use image_src if ogp::image isn't set.
+    if (!isset($page->values['image'])) {
+      $domxpath = new DOMXPath($doc);
+      $elements = $domxpath->query("//link[@rel='image_src']");
+
+      if ($elements->length > 0) {
+        $domattr = $elements->item(0)->attributes->getNamedItem('href');
+        if ($domattr) {
+          $page->_values['image'] = $domattr->value;
+          $page->_values['image_src'] = $domattr->value;
         }
+      }
+    }
 
     if (empty($page->_values)) { return false; }
 
